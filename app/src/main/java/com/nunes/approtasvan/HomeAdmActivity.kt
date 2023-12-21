90c0a4c2930fcb2d0a42c6faea1a0852a37b65d8
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2023-12-21 18:07:16 -0300

    criando telas de home do aluno e do adm e de cadastrar aluno

---
+++ app/src/main/java/com/nunes/approtasvan/HomeAdmActivity.kt
@@ -0,0 +1,11 @@
+package com.nunes.approtasvan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeAdmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_adm)
+    }
+}
---
+++ app/src/main/java/com/nunes/approtasvan/HomeAlunoActivity.kt
@@ -0,0 +1,11 @@
+package com.nunes.approtasvan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HomeAlunoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home_aluno)
+    }
+}

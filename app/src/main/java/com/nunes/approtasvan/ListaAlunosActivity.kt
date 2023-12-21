e5fcdda8ce519f5fad43bcd9f8686075b2244e59
Matheus Nunes <matheusnunes.sobrinho18@gmail.com>
2023-12-21 18:31:37 -0300

    criando tela para listar alunos

---
+++ app/src/main/java/com/nunes/approtasvan/ListaAlunosActivity.kt
@@ -0,0 +1,11 @@
+package com.nunes.approtasvan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListaAlunosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lista_alunos)
+    }
+}
